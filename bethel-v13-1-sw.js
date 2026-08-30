f1c9f08c03f1e5882685b613d2dc058fd04ba40c
mhodish <62631750+mhodish@users.noreply.github.com>
2026-08-30 08:21:38 -0400

    Add files via upload

---
+++ bethel-v13-1-sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'bethel-roadbook-v13-1';
+const APP_SHELL = [
+  './Bethel_Roadbook_V13_1_Offline_Diagnostic.html',
+  './bethel-v13-1.webmanifest',
+  './bethel-v13-1-icon-192.png',
+  './bethel-v13-1-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k.startsWith('bethel-roadbook-v13-') && k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('./Bethel_Roadbook_V13_1_Offline_Diagnostic.html')
+        .then(cached => cached || fetch(event.request))
+        .catch(() => caches.match('./Bethel_Roadbook_V13_1_Offline_Diagnostic.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true})
+      .then(cached => cached || fetch(event.request))
+  );
+});
